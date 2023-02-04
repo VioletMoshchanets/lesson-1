c4247482eb48c4ca5e6013fa0a0b81eee994f6ad
Violetiiik <123232193+VioletMoshchanets@users.noreply.github.com>
2023-02-04 09:26:12 +0000

    mood

--- main.py
+++ main.py
@@ -1,24 +1,73 @@
-try:
-  a = int(input('A: '))
-  sign = input('Sign: ')
-  b = int(input('B: '))
-  if sign == '/' and b == 0:
-    raise ZeroDivisionError
-  if sign != '+' and sign != '-' and sign!='*' and sign!='/':
-    raise Warning
-  elif sign == '+':
-    print(f"{a} + {b} = {a+b}")
-  elif sign == '-':
-    print(f"{a} - {b} = {a-b}")
-  elif sign == '*':
-    print(f"{a} * {b} = {a*b}")
-  elif sign == '*':
-    print(f"{a} / {b} = {a/b}")
-except ValueError:
-  print('You need to input digits!')
-except ZeroDivisionError:
-  print('Go to school')
-except Warning:
-  print('Unknown sign')
-except:
-  print('Error!')
+def happy(func):
+  def wrapper():
+    print('I am happy')
+    func()
+  return wrapper
+
+def sad(func):
+  def wrapper():
+    print('I am sad')
+    func()
+  return wrapper
+
+def angry(func):
+  def wrapper():
+    print('I am angry')
+    func()
+  return wrapper
+
+def scary(func):
+  def wrapper():
+    print('I am scary')
+    func()
+  return wrapper
+
+def embarrassed(func):
+  def wrapper():
+    print('I am embarrassed')
+    func()
+  return wrapper
+
+def amazed(func):
+  def wrapper():
+    print('I am amazed')
+    func()
+  return wrapper
+
+def suprised(func):
+  def wrapper():
+    print('I am surprised')
+    func()
+  return wrapper
+
+def good(func):
+  def wrapper():
+    print('I am good')
+    func()
+  return wrapper
+
+def disappointed(func):
+  def wrapper():
+    print('I am disappointed')
+    func()
+  return wrapper
+
+def disgust(func):
+  def wrapper():
+    print('I feel disgust')
+    func()
+  return wrapper
+
+@happy
+@sad
+@angry
+@scary
+@embarrassed
+@amazed
+@suprised
+@good
+@disappointed
+@disgust
+def human():
+  print('I am human')
+human()
