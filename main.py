78690f96d6f5b78128bbf4575c361f0870a4e05e
Violetiiik <123232193+VioletMoshchanets@users.noreply.github.com>
2023-02-07 08:54:59 +0000

    1234567890

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 class name:
-  user_name = int(input('Your name: '))
-  'One1'.isalpha() #=> False
-  'One'.isalpha() 
+  def main():
+    name = input("What is your name?: ")
+    for i in name:
+        if i.isdigit():
+            raise ValueError("The name must not contain numbers!") 
   
   
 class calculator:
-  try:
-    a = int(input('A: '))
-    sign = input('Sign: ')
-    b = int(input('B: '))
-    if sign == '/' and b == 0:
-      raise ZeroDivisionError
-    if sign != '+' and sign != '-' and sign!='*' and sign!='/':
-      raise Warning
-    elif sign == '+':
-      print(f"{a} + {b} = {a+b}")
-    elif sign == '-':
-      print(f"{a} - {b} = {a-b}")
-    elif sign == '*':
-      print(f"{a} * {b} = {a*b}")
-    elif sign == '*':
-      print(f"{a} / {b} = {a/b}")
-  except ValueError:
-    print('You need to input digits!')
-  except ZeroDivisionError:
-    print('Go to school')
-  except Warning:
-    print('Unknown sign')
-  except:
-    print('Error!')
+  def main2():
+    try:
+      a = int(input('A: '))
+      sign = input('Sign: ')
+      b = int(input('B: '))
+      if sign == '/' and b == 0:
+        raise ZeroDivisionError
+      if sign != '+' and sign != '-' and sign!='*' and sign!='/':
+        raise Warning
+      elif sign == '+':
+        print(f"{a} + {b} = {a+b}")
+      elif sign == '-':
+        print(f"{a} - {b} = {a-b}")
+      elif sign == '*':
+        print(f"{a} * {b} = {a*b}")
+      elif sign == '*':
+        print(f"{a} / {b} = {a/b}")
+    except ValueError:
+      print('You need to input digits!')
+    except ZeroDivisionError:
+      print('Go to school')
+    except Warning:
+      print('Unknown sign')
+    except:
+      print('Error!')
+
+obj1 = name()
+obj2 = calculator()
